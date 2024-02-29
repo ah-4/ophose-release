06ee1fae71183e211f2435f0c29b13e13341eb91
ah-4 <gv2ni.pro@gmail.com>
2024-02-29 18:46:29 +0100

    Security enhanced: Added Header to prevent CORS

--- ophose/app/request/security.php
+++ ophose/app/request/security.php
@@ -1,6 +1,8 @@
 <?php
 if (session_status() === PHP_SESSION_NONE) session_start();
 
+header("Access-Control-Allow-Origin: *");
+
 $SECURITY_SESSION_TTL_IN_MINUTES = 1440; // 24 hours
 
 if (!isset($_SESSION["CSRF_TOKEN"]) || (isset($_SESSION["CSRF_TOKEN"]) && (time() - $_SESSION["CSRF_TOKEN_TIME"]) > $SECURITY_SESSION_TTL_IN_MINUTES * 60)) {
